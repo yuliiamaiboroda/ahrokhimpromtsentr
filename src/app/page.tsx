2fc14cfbe58dc7c0ad423f6357ad7802712ed29a
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-18 21:51:04 +0300

    Add images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import AdvantagesSection from '@/components/AdvantagesSection';
+import MainHero from '@/components/MainHero';
 
 export default function Home() {
   return (
-    <div className="px-4 py-2  font-xl border-2 border-solid border-accent shadow-md">
+    <div className="px-4 py-2">
       Its MAIN page
+      <MainHero />
       <AdvantagesSection />
     </div>
   );
