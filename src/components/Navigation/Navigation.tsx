e5a07f346d1f168a5e9216a46af882ceab74e45e
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-20 11:18:53 +0300

    Styled navigation bar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,7 +2,6 @@
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
-import css from './Navigation.module.css';
 
 const NAVIGATION_LINKS = [
   { href: '/', title: 'Головна' },
@@ -18,24 +17,44 @@ export default function Navigation() {
 
   return (
     <nav>
-      <ul className="flex  flex-wrap  justify-center  gap-4 p-4">
+      <ul
+        className="hidden flex-wrap  justify-center gap-9 
+                    p-4 md:flex xl:gap-20"
+      >
         {NAVIGATION_LINKS.map(({ href, title }) => (
           <li
             key={href}
-            className="relative [&:not(:last-child)]:after:absolute [&:not(:last-child)]:after:bottom-[6px] [&:not(:last-child)]:after:left-[calc(50%+6px)] [&:not(:last-child)]:after:h-0 [&:not(:last-child)]:after:w-1/2 [&:not(:last-child)]:after:border [&:not(:last-child)]:after:border-solid [&:not(:last-child)]:after:border-black
-                        [&:not(:last-child)]:after:content-[''] [&:not(:first-child)]:before:absolute [&:not(:first-child)]:before:bottom-[6px] [&:not(:first-child)]:before:right-[calc(50%+6px)] [&:not(:first-child)]:before:h-0 [&:not(:first-child)]:before:w-1/2 [&:not(:first-child)]:before:border [&:not(:first-child)]:before:border-solid [&:not(:first-child)]:before:border-black
-                        [&:not(:first-child)]:before:content-['']
+            className="relative 
+            [&:not(:first-child)]:before:absolute
+            [&:not(:first-child)]:before:bottom-[6px]
+            [&:not(:first-child)]:before:right-[calc(50%+6px)]
+            [&:not(:first-child)]:before:h-0
+            [&:not(:first-child)]:before:w-[calc(50%+13px)]
+            [&:not(:first-child)]:before:border
+            [&:not(:first-child)]:before:border-solid
+            [&:not(:first-child)]:before:border-navigation
+            xl:[&:not(:first-child)]:before:w-[calc(50%+35px)]
+            [&:not(:last-child)]:after:absolute
+            [&:not(:last-child)]:after:bottom-[6px]
+            [&:not(:last-child)]:after:left-[calc(50%+6px)]
+            [&:not(:last-child)]:after:h-0
+            [&:not(:last-child)]:after:w-[calc(50%+13px)]
+            [&:not(:last-child)]:after:border
+            [&:not(:last-child)]:after:border-solid
+            [&:not(:last-child)]:after:border-navigation
+            xl:[&:not(:last-child)]:after:w-[calc(50%+35px)]
                         "
           >
             <Link
               href={href}
-              className="text-shadow-navigation relative flex flex-col items-center font-body text-lg
-                         font-bold text-navigation "
+              className="text-shadow-navigation flex flex-col items-center 
+                        gap-1.5 font-body text-lg font-bold text-navigation
+                        xl:gap-1 xl:text-xl "
             >
               {title}
               <svg
-                className={`h-[14px] w-[14px] stroke-black ${
-                  pathname === href ? 'fill-black' : 'fill-none'
+                className={`h-3.5 w-3.5 stroke-navigation ${
+                  pathname === href ? 'fill-navigation' : 'fill-none'
                 }`}
               >
                 <use href="/images/icons.svg#point-outline"></use>
