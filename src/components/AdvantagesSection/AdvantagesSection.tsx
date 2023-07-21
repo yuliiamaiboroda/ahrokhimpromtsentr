bc3c2eb1a2d9ab27134ee72acb59b13054d6e130
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-07-21 11:55:03 +0300

    fix y-scroll

--- src/components/AdvantagesSection/AdvantagesSection.tsx
+++ src/components/AdvantagesSection/AdvantagesSection.tsx
@@ -1,36 +1,23 @@
-// import Image, { StaticImageData } from 'next/image';
 import AdvantagesList from '../AdvantagesList';
 import ButtonLink from '../ButtonLink';
-// import bgRightBottom from '../../../public/images/right-bottom-image@3x.png';
-// import bgLeftBottom from '../../../public/images/left-bottom-image@3x.png';
-
-// function Background({ src }: { src: StaticImageData }) {
-//   return (
-//     <Image
-//       src={src}
-//       alt="hero"
-//       placeholder="blur"
-//       quality={100}
-//       width={300}
-//       height={600}
-//       style={{
-//         objectFit: 'cover',
-//       }}
-//     />
-//   );
-// }
+import bgRightBottom from '../../../public/images/right-bottom-image@3x.png';
+import bgLeftBottom from '../../../public/images/left-bottom-image@3x.png';
+import BackgroundImage from '../BackgroundImage';
+BackgroundImage;
 
 export default function AdvantagesSection() {
   return (
-    <section>
+    <section className="relative overflow-hidden">
       <div className="flex flex-col	items-center gap-12 py-20 pt-[15rem] md:gap-16 md:py-28	xl:gap-[72px] xl:pb-20 xl:pt-12">
         <AdvantagesList />
         <ButtonLink navigateTo="/about" title="Більше про нас" size="l" />
       </div>
-      {/* <>
-        <Background src={bgRightBottom} />
-        <Background src={bgLeftBottom} />
-      </> */}
+      {/* <div className="absolute -bottom-[10%] left-0 -z-[1] h-[500px] w-[250px] -translate-x-1/2 blur-[0.9559948444366455px] xl:h-full xl:w-[350px]">
+        <BackgroundImage src={bgLeftBottom} alt="background plants" />
+      </div>
+      <div className="absolute -bottom-[10%] right-0 -z-[1] h-[500px] w-[250px] translate-x-1/2 blur-[0.9559948444366455px]  xl:h-full xl:w-auto">
+        <BackgroundImage src={bgRightBottom} alt="background plants" />
+      </div> */}
     </section>
   );
 }
