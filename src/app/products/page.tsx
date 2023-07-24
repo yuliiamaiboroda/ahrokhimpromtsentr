1c8cc5be1ebfecfc48a9af133d378b6f66820ad7
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-24 14:22:37 +0300

    Create offers gallery and add it to products and services

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import SectionTitle from '@/components/SectionTitle';
+import OfferingsGallery from '@/components/OfferingsGallery';
 
 interface IProductItem {
   _id: string;
@@ -22,44 +23,17 @@ async function getProducts() {
   return res.json() as Promise<IProductItem[]>;
 }
 
-// TODO: Add correct styles for SectionTitle and Section (paddings and margins), separate Section and Container to different reusable components;
+// TODO: Add correct styles for SectionTitle and Section (paddings and margins);
 
 export default async function Page() {
-  const data = await getProducts();
+  const products = await getProducts();
 
   return (
     <>
       <section className="pb-16 md:pb-32 xl:pb-40">
         <SectionTitle title="Продукти компанії" />
         <div className="mx-auto mt-9 max-w-[480px] px-4 md:mt-12 md:max-w-[755px] xl:mt-20 xl:max-w-[810px]">
-          <ul className="flex flex-wrap items-center justify-center gap-6 md:gap-16">
-            {data.map(({ _id, title, imageURL }) => (
-              <li
-                key={_id}
-                className="overflow-hidden rounded-[13px] bg-light-gradient p-0.5 shadow-product hover:shadow-hover"
-              >
-                <Link
-                  href={'/products/' + _id}
-                  className="relative block h-[186px] w-[266px] overflow-hidden 
-                              rounded-[13px] md:h-[226px] md:w-[323px] xl:w-[350px]"
-                >
-                  <Image
-                    src={imageURL}
-                    alt={title}
-                    fill
-                    sizes="(min-width: 1440px) 25vh,  (min-width: 1280px) 33vh,  (min-width: 768px) 50vh,  100vh"
-                    className="object-cover"
-                  />
-                  <p
-                    className="absolute left-0 top-1/2 w-full -translate-y-1/2 bg-[rgba(47,_64,_48,_0.67)] p-3 
-                                text-center font-body text-base font-bold md:p-2 md:text-xl xl:text-2xl"
-                  >
-                    {title}
-                  </p>
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <OfferingsGallery offers={products} navigateTo="/products" />
         </div>
       </section>
     </>
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,6 +3,7 @@ import SectionTitle from '@/components/SectionTitle';
 import ButtonLink from '@/components/ButtonLink';
 import Section from '@/components/Section';
 import Container from '@/components/Container';
+import OfferingsGallery from '@/components/OfferingsGallery';
 
 interface IServiceItem {
   _id: string;
@@ -34,49 +35,7 @@ export default async function Page() {
       <Section>
         <Container>
           <SectionTitle title="Послуги" />
-          <ul className="font-body font-semibold">
-            {services.map(({ _id, title, description, imageURL, price }) => (
-              <li
-                key={_id}
-                className="mx-auto mb-9 flex w-full max-w-[266px] flex-col gap-5 last:mb-0 
-                          md:mb-16 md:max-w-full md:flex-row md:gap-8 xl:mb-20 xl:gap-10"
-              >
-                <div
-                  className="golden-edge relative h-[186px] w-full shrink-0 md:h-[225px]
-                                md:w-[276px] xl:h-[256px] xl:w-[350px]"
-                >
-                  <Image
-                    src={imageURL}
-                    alt={title}
-                    fill
-                    sizes="(min-width: 1280px) 350px, (mi-width: 768px) 276px, 226px"
-                    className="rounded-xl object-cover object-center"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <h3
-                    className="relative mx-auto mb-5 w-fit text-xl before:absolute before:left-0 before:top-full
-                                 before:h-1 before:w-full before:rounded-sm before:bg-underline-gradient
-                                  md:mx-0 xl:text-2xl"
-                  >
-                    {title}
-                  </h3>
-                  <p className="text-center text-base md:text-left md:text-lg xl:text-xl">
-                    {description.length <= 130
-                      ? description
-                      : description.slice(0, 127) + '...'}
-                  </p>
-                  <div className="mx-auto mt-6 w-fit md:mx-0 md:mt-auto">
-                    <ButtonLink
-                      navigateTo={`/services/${_id}`}
-                      title="Зв‘язатись з нами"
-                      size="s"
-                    />
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <OfferingsGallery offers={services} navigateTo="/services" />
         </Container>
       </Section>
     </>
