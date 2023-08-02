22c305f7d33aad9c7ed2c98213423452ad40e04d
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-08-02 10:46:34 +0300

    Styled checkbox input

--- src/components/CheckboxField/CheckboxField.tsx
+++ src/components/CheckboxField/CheckboxField.tsx
@@ -14,15 +14,24 @@ interface IProps extends InputHTMLAttributes<HTMLInputElement> {
 export default function CheckboxField({ label, name }: IProps) {
   const [field, meta] = useField(name);
   return (
-    <label className="relative flex cursor-pointer items-center gap-2">
+    <label
+      className="relative flex cursor-pointer items-center gap-4
+                text-base font-semibold text-placeholder
+                transition duration-200 hover:text-primary
+                md:text-xl xl:text-2xl [&>div]:hover:shadow-hover"
+    >
       <input {...field} type="checkbox" className="absolute -z-0 scale-0" />
-      <div className="relative h-6 w-6 rounded-[4px] border-2 border-solid border-accent ">
+      <div
+        className="relative h-9 w-9 shrink-0 rounded-md 
+                    border-2 border-solid border-accent 
+                    transition duration-200 md:h-10 md:w-10 "
+      >
         <svg
           className={`${
             field.value ? 'opacity-100' : 'opacity-0'
-          } absolute left-1/2 top-1/2 h-6 w-6 
-          -translate-x-1/3 -translate-y-[60%] fill-accent stroke-secondary
-          transition duration-200`}
+          } absolute left-1/2 top-1/2 h-9 w-9 -translate-x-1/3 -translate-y-[60%]
+          fill-accent stroke-secondary transition duration-200
+          md:h-10 md:w-10`}
         >
           <use href="/images/icons.svg#check-mark"></use>
         </svg>
