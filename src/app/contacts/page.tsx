c617380cbcd7170e2fe0b17d23f20dd4b2c71013
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-08-03 12:36:32 +0300

    Add post feedback and add title for form

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -3,6 +3,8 @@ import HeroSection from '@/components/HeroSection';
 import FeedbackForm from '@/components/FeedbackForm';
 import Section from '@/components/Section/Section';
 import Container from '@/components/Container/Container';
+import SectionTitle from '@/components/SectionTitle';
+import Description from '@/components/Description';
 
 export default function Page() {
   return (
@@ -13,6 +15,10 @@ export default function Page() {
       />
       <Section>
         <Container>
+          <SectionTitle title="Відгукнутися" />
+          <div className="mx-auto w-4/5 md:w-1/2">
+            <Description description="Вкажіть свої контактні дані і ми надамо зворотній зв’язок" />
+          </div>
           <FeedbackForm />
         </Container>
       </Section>
