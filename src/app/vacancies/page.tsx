817d06086498f69776454997644863b2b7aa73d9
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-08-14 19:20:25 +0300

    Add work descriptions

--- src/app/vacancies/page.tsx
+++ src/app/vacancies/page.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import vacanciesBackground from '../../../public/images/bg-vacancies-screen.jpg';
 import HeroSection from '@/components/HeroSection';
 import Section from '@/components/Section';
@@ -7,7 +6,7 @@ import SectionTitle from '@/components/SectionTitle';
 import Description from '@/components/Description';
 import SmallCotainer from '@/components/SmallContainer';
 import ResumeForm from '@/components/ResumeForm';
-// import Notification from '@/components/Notification';
+import ButtonLink from '@/components/ButtonLink';
 
 interface IVacancyTitle {
   _id: string;
@@ -33,22 +32,34 @@ export default async function Page() {
   return (
     <main>
       <HeroSection src={vacanciesBackground} alt="Поля в гірській місцевості" />
-      {/* {isNotificationOpen && (
-        <Notification
-          setIsNotificationOpen={setIsNotificationOpen}
-          isNotificationOpen={isNotificationOpen}
-          status="success"
-        />
-      )} */}
       <Section>
         <Container>
-          <SectionTitle title="Відгукнутися" />
-          <SmallCotainer>
-            <Description description="Вкажіть свої контактні дані і ми надамо зворотній зв’язок" />
-          </SmallCotainer>
-          <ResumeForm vacancies={vacancyTitles} />
+          <SectionTitle title="Робота" />
+          <div className="flex flex-col gap-5">
+            <Description description="Підприємство з багаторічним досвідом знаходиться у постійному пошуку кваліфікованих спеціалістів у різних галузях сільськогосподарського направлення. Майбутнім працівникам пропонується ряд переваг: гідна заробітна плата, повний соціальний пакет та якісні умови роботи." />
+            <Description description="Претендентам достатньо залишити онлайн заявку на сайті, щоб отримати швидкий зворотний зв'язок. Просимо звернути увагу, що відділ кадрів доступний виключно у робочий час. " />
+            <Description description="Бажаєте приєднатись?" />
+            <Description description="Дізнайтесь про відкриті вакансії прямо зараз!" />
+          </div>
+          <div className="mx-auto mt-5 w-fit md:mt-10">
+            <ButtonLink
+              navigateTo="/vacancies#resumeform"
+              title="Залишити заявку"
+            />
+          </div>
         </Container>
       </Section>
+      <div id="resumeform">
+        <Section>
+          <Container>
+            <SectionTitle title="Відгукнутися" />
+            <SmallCotainer>
+              <Description description="Вкажіть свої контактні дані і ми надамо зворотній зв’язок" />
+            </SmallCotainer>
+            <ResumeForm vacancies={vacancyTitles} />
+          </Container>
+        </Section>
+      </div>
     </main>
   );
 }
