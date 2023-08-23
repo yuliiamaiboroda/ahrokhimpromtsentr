df8fedd1025e623517230dbc1eb5fce77a183013
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-08-23 09:39:56 +0300

    Refactored button component and refactored forms buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,8 @@
 import { ButtonsVariants } from '@/helpers/constants';
+import { ButtonHTMLAttributes } from 'react';
 
-interface IProps {
-  type: 'button' | 'submit' | 'reset';
-  title: string;
-  isDisabled?: boolean;
+interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: keyof typeof ButtonsVariants;
-  onClick: () => void;
 }
 
 // Variants: primary : Кнопка повністю залита золотим градієнтом,secondary: Кнопка залалита темним градієнтом і обведена золотистою рамкою
@@ -13,27 +10,27 @@ interface IProps {
 // TODO discuss about disabled variant for secondary vsriant
 
 export default function Button({
-  type,
-  title,
-  isDisabled = false,
+  type = 'button',
+  disabled = false,
   variant = ButtonsVariants.primary,
-  onClick,
+  children,
+  ...props
 }: IProps) {
   return (
-    <div className={`${isDisabled ? 'blur-[2px]' : ''}`}>
+    <div className={`${disabled ? 'blur-[2px]' : ''}`}>
       <button
         type={type}
-        onClick={onClick}
-        disabled={isDisabled ? true : false}
+        disabled={disabled}
+        {...props}
         className={` flex w-[178px] items-center justify-center rounded-xl py-3 font-body text-sm font-bold md:w-[302px]  md:py-5 md:text-2xl xl:w-[272px] xl:py-6	xl:text-[28px]
         ${
           variant === ButtonsVariants.primary
             ? 'primary__button'
             : 'secondary__button'
         }   
-      ${isDisabled ? 'hover:shadow-none' : ''}`}
+      ${disabled ? 'hover:shadow-none' : ''}`}
       >
-        {title}
+        {children}
       </button>
     </div>
   );
