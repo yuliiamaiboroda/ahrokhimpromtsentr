d4e76e38e025b34dafbfbdd1c97c7c49952c9080
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-09-13 15:46:16 +0300

    fix styles for mobile

--- src/components/ListOneColumn/ListOneColumn.tsx
+++ src/components/ListOneColumn/ListOneColumn.tsx
@@ -19,14 +19,14 @@ export default function ListOneColumn({ list, title = '' }: IProps) {
         {list.map((text, index) => (
           <li
             key={index}
-            className="relative  grid grid-cols-[10px_1fr]	items-center gap-2.5	md:gap-5 xl:gap-6"
+            className="relative grid grid-cols-[2px_1fr] items-center gap-2.5 md:gap-5 xl:gap-6 "
           >
             <Image
               src={ColossusIcon}
               alt="colossus"
               className="absolute -left-1.5 -top-2.5 h-6 w-6 md:h-7 md:w-7 xl:h-8 xl:w-8"
             />
-            <span className="block h-3/4 w-0.5	bg-light-gradient  md:mt-[15px] "></span>
+            <span className="block h-3/4 w-[1px]	bg-light-gradient  md:mt-[15px] md:w-0.5 "></span>
             <span className="py-5  font-body text-base font-bold	text-primary md:py-9	md:text-xl xl:text-2xl">
               {text}
             </span>
