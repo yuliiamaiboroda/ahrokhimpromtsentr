177e2ab50f1155a13acf38e86a642eaaa838e184
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-09-13 15:24:17 +0300

    create common component for list

--- src/components/AdvantagesList/AdvantagesList.tsx
+++ src/components/AdvantagesList/AdvantagesList.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image';
 import ColossusIcon from 'public/images/colossus.svg';
 
-// TODO: rewrite on real text  , check sizes, rewrite on svg sprite
-
 export default function AdvantagesList() {
   return (
     <ul className="mx-auto  grid	max-w-[210px]  md:max-w-2xl  md:grid-cols-2 md:gap-x-[76px] xl:max-w-[752px] ">
@@ -12,7 +10,7 @@ export default function AdvantagesList() {
           alt="colossus"
           className="absolute -left-1.5 -top-2.5 h-6 w-6 md:h-7 md:w-7 xl:h-8 xl:w-8"
         />
-        <span className="block h-3/4	 w-[4px]	bg-light-gradient  md:mt-[15px] md:w-1 "></span>
+        <span className="block h-3/4	 w-[4px]	bg-light-gradient  md:mt-[15px] "></span>
         <span className="py-5  font-body text-base font-bold	text-primary md:py-9	md:text-xl xl:text-2xl">
           Активний розвиток та збільшення технологічних можливостей протягом 24
           років.
@@ -24,7 +22,7 @@ export default function AdvantagesList() {
           alt="colossus"
           className="absolute -left-1.5 -top-2.5 h-6 w-6 md:h-7 md:w-7 xl:h-8 xl:w-8"
         />
-        <span className="block h-3/4  w-[4px]	bg-light-gradient  md:mt-[15px] md:w-1 "></span>
+        <span className="block h-3/4  w-[4px]	bg-light-gradient  md:mt-[15px] "></span>
         <span className="py-5  font-body text-base font-bold	text-primary md:py-9	md:text-xl xl:text-2xl">
           Дотримання вікових традицій у поєднанні з сучасними принципами, що
           гарантує якісний результат.
@@ -36,7 +34,7 @@ export default function AdvantagesList() {
           alt="colossus"
           className="absolute -left-1.5 -top-2.5 h-6 w-6 md:h-7 md:w-7 xl:h-8 xl:w-8"
         />
-        <span className="block h-3/4	 w-[4px]	bg-light-gradient  md:mt-[15px] md:w-1 "></span>
+        <span className="block h-3/4	 w-[4px]	bg-light-gradient  md:mt-[15px] "></span>
         <span className="py-5  font-body text-base font-bold	text-primary md:py-9	md:text-xl xl:text-2xl">
           Потужності та відповідна техніка для вирощування, збору, оброблення та
           зберігання продукції.
@@ -48,7 +46,7 @@ export default function AdvantagesList() {
           alt="colossus"
           className="absolute -left-1.5 -top-2.5	h-6 w-6 md:h-7 md:w-7 xl:h-8 xl:w-8"
         />
-        <span className="block h-3/4	 w-[4px]	bg-light-gradient  md:mt-[15px] md:w-1 "></span>
+        <span className="block h-3/4	 w-[4px]	bg-light-gradient  md:mt-[15px] "></span>
         <span className="py-5  font-body text-base font-bold	text-primary md:py-9	md:text-xl xl:text-2xl">
           Вагома праця для збереження історичних цінностей сільського
           господарства України.
