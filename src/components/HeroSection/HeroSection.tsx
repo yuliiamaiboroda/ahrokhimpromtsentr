f210b42a15596813e1df1a082cdd7e2086c1ad91
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-09-18 17:48:20 +0300

    Set background color for hero section

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -10,7 +10,7 @@ export default function HeroSection({
   alt: string;
 }) {
   return (
-    <section className="relative h-[218px] w-full overflow-hidden md:h-[537px] xl:h-[628px]">
+    <section className="relative h-[218px] w-full overflow-hidden bg-accent md:h-[537px] xl:h-[628px]">
       <BackgroundImage src={src} alt={alt} placeholder="blur" priority />
       <HeroTitle />
     </section>
