ae2d8619bd420ddd614ac7ce3c937f04ffb81c72
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-10-30 14:07:15 +0200

    Set revalidation time

--- src/helpers/constants/config.ts
+++ src/helpers/constants/config.ts
@@ -1,6 +1,4 @@
-export const REVALIDATION_DELAY = 10;
+export const REVALIDATION_DELAY = 600;
 
 export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:5050';
-
-// ahrokhimpromtsentr.cyclic.app
+  process.env.NEXT_PUBLIC_BASE_URL || 'https://ahrokhimpromtsentr.cyclic.app';
